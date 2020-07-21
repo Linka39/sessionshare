fd9042752978e1523eb947aed8307099c7bf95c7
linka39 <1203440758@qq.com>
2020-07-21 17:49:57 +0800

    v1.001 1、引入并使用quartz 2、quartz实现对job的管理

---
+++ src/main/java/com/linka39/util/DatabaseBackupJob.java
@@ -0,0 +1,21 @@
+package com.linka39.util;
+
+import org.quartz.Job;
+import org.quartz.JobDetail;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class DatabaseBackupJob implements Job {
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        JobDetail detail = context.getJobDetail();
+        String database = detail.getJobDataMap().getString("database");
+
+        System.out.printf("给数据库 %s 备份, 耗时4秒 %n" ,database);
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/linka39/util/StoppableJob.java
@@ -0,0 +1,30 @@
+package com.linka39.util;
+
+import org.quartz.InterruptableJob;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.UnableToInterruptJobException;
+
+public class StoppableJob implements InterruptableJob {
+    private boolean stop = false;
+    @Override
+    public void interrupt() throws UnableToInterruptJobException {
+        System.out.println("被调度叫停");
+        stop = true;
+    }
+
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        while(true){
+            if(stop)
+                break;
+            try{
+                System.out.println("每隔1秒，进行一次检测，看看是否停止");
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            System.out.println("持续工作中。。。");
+        }
+    }
+}
